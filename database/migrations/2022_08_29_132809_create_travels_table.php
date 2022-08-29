0941ca12df43f86419f040d52d3ccc7a34c4408b
Binotz <binotto.riccardo@gmail.com>
2022-08-29 15:58:36 +0200

    first commit

---
+++ database/migrations/2022_08_29_132809_create_travels_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            //definisco le colonne della tabella
+            $table->id();
+            $table->string('partenza', 15);
+            $table->string('destinazione', 15);
+            $table->date('orario_partenza');
+            $table->date('orario_arrivo');
+            $table->unsignedDecimal('prezzo', 7,2);
+            $table->smallInteger('numero_camere');
+            $table->smallInteger('numero_persone');
+            $table->smallInteger('wifi_gratis')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.app')
+
+@section('main-content')
+    <h1>Laravel Viaggi</h1>
+@endsection
